60edb3afa989148c8bd8da6b873b5a4e66ed9b00
damian <qaqa.tanski@users.noreply.github.com>
2023-02-22 16:34:15 +0100

    Adding "edit owner"

--- src/main/java/com/apartments/base/owner/mapper/OwnerMapper.java
+++ src/main/java/com/apartments/base/owner/mapper/OwnerMapper.java
@@ -1,5 +1,6 @@
 package com.apartments.base.owner.mapper;
 
+import com.apartments.base.owner.models.EditOwnerDto;
 import com.apartments.base.owner.models.NewOwnerDto;
 import com.apartments.base.owner.models.Owner;
 import com.apartments.base.utils.models.Address;
@@ -24,6 +25,23 @@ public class OwnerMapper {
                 .surname(newOwnerDto.surname())
                 .lastname(newOwnerDto.lastname())
                 .phoneNumber(newOwnerDto.phoneNumber())
+                .address(address)
+                .build();
+    }
+
+    public static Owner toOwner(EditOwnerDto editOwnerDto) {
+        Objects.requireNonNull(editOwnerDto, "Input cannot be null");
+
+        Address address = Address.builder()
+                .city(editOwnerDto.city())
+                .street(editOwnerDto.street())
+                .postcode(editOwnerDto.postcode())
+                .build();
+
+        return Owner.builder()
+                .surname(editOwnerDto.surname())
+                .lastname(editOwnerDto.lastname())
+                .phoneNumber(editOwnerDto.phoneNumber())
                 .address(address)
                 .build();
     }
---
+++ src/main/java/com/apartments/base/owner/models/EditOwnerDto.java
@@ -0,0 +1,36 @@
+package com.apartments.base.owner.models;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record EditOwnerDto (
+        @Schema(example = "Adrian",
+                type = "string",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                maxLength = 30,
+                minLength = 1
+        ) String surname,
+
+        @Schema(example = "Kowalski",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                maxLength = 30,
+                minLength = 1)
+        String lastname,
+
+        @Schema(example = "999999999",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                maxLength = 9,
+                minLength = 9)
+        String phoneNumber,
+        @Schema(example = "London",
+                requiredMode = Schema.RequiredMode.REQUIRED)
+        String city,
+
+        @Schema(example = "Avenue",
+                requiredMode = Schema.RequiredMode.REQUIRED)
+        String street,
+
+        @Schema(example = "123",
+                requiredMode = Schema.RequiredMode.REQUIRED)
+        String postcode) {
+
+}
